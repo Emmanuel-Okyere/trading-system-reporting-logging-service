2b8487665710d3703011a43ffbd19c35d1c578ec
Emmanuel Ametepee <emmanuel.metepee@turntabl.io>
2022-11-22 17:53:29 +0000

    Init commit

---
+++ src/test/java/com/tlc/group/seven/reportingloggingservice/ReportingLoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlc.group.seven.reportingloggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingLoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
