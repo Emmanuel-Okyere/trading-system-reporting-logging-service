3378ce999199e20f1cc4e2ded75d0c95a70bf75f
Emmanuel Ametepee <emmanuel.metepee@turntabl.io>
2022-12-01 08:04:25 +0000

    endpoint fixed

--- src/main/java/com/tlc/group/seven/reportingloggingservice/logs/controller/LogDataController.java
+++ src/main/java/com/tlc/group/seven/reportingloggingservice/logs/controller/LogDataController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/admin/reporting")
+@RequestMapping("/api/v1/admin/reports")
 public class LogDataController {
     @Autowired
     private LogDataService dataService;
