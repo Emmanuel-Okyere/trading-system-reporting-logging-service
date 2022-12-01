27c3ccb654a81e6fb6d7d1d91d8490ca2dafc4c5
Emmanuel Ametepee <emmanuel.metepee@turntabl.io>
2022-12-01 12:16:57 +0000

    code refactored

--- src/main/java/com/tlc/group/seven/reportingloggingservice/order/model/Order.java
+++ src/main/java/com/tlc/group/seven/reportingloggingservice/order/model/Order.java
@@ -2,6 +2,7 @@ package com.tlc.group.seven.reportingloggingservice.order.model;
 
 import com.tlc.group.seven.reportingloggingservice.user.model.User;
 import lombok.Data;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -11,6 +12,7 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "orders")
+@ToString
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,22 +27,6 @@ public class Order {
     private String orderId;
     private Date createdAt;
     private Date updatedAt;
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "iD=" + iD +
-                ", product='" + product + '\'' +
-                ", quantity=" + quantity +
-                ", price=" + price +
-                ", type='" + type + '\'' +
-                ", side='" + side + '\'' +
-                ", orderId='" + orderId + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                ", users=" + users +
-                '}';
-    }
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "users_id")
