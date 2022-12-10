202765a17f3d7da59b07c3027330ecda060de95c
Manuel Amet <emmanuel.metepee@turntabl.io>
2022-12-10 00:44:48 +0000

    order model updated

--- src/main/java/com/tlc/group/seven/reportingloggingservice/order/model/Order.java
+++ src/main/java/com/tlc/group/seven/reportingloggingservice/order/model/Order.java
@@ -1,14 +1,8 @@
 package com.tlc.group.seven.reportingloggingservice.order.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.tlc.group.seven.reportingloggingservice.portfolio.model.Portfolio;
-import com.tlc.group.seven.reportingloggingservice.user.model.User;
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import lombok.Data;
 import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -33,12 +27,11 @@ public class Order {
     private String side;
 
     private String orderId;
+    private String orderStatus;
 
     private Date createdAt;
 
     private Date updatedAt;
-
-
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "portfolio_id")
