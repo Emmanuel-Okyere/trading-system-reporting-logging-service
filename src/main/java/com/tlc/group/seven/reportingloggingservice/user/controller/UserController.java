1dd54c0270ca67eeea2530c17ff6e5c33abdc33a
Emmanuel Ametepee <emmanuel.metepee@turntabl.io>
2022-12-12 08:34:02 +0000

    code updated

--- src/main/java/com/tlc/group/seven/reportingloggingservice/user/controller/UserController.java
+++ src/main/java/com/tlc/group/seven/reportingloggingservice/user/controller/UserController.java
@@ -1,15 +1,11 @@
 package com.tlc.group.seven.reportingloggingservice.user.controller;
 
-import com.tlc.group.seven.reportingloggingservice.user.model.User;
 import com.tlc.group.seven.reportingloggingservice.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
